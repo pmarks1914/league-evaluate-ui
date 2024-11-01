e57668b5a095afbe470c7cfec828e48ae8641381
Patrick <pmarks1914@gmail.com>
2024-11-01 23:40:47 +0000

    clean

--- src/views/pages/Application/ApplicationDataTables.js
+++ src/views/pages/Application/ApplicationDataTables.js
@@ -30,7 +30,6 @@ const ApplicationDataTables = () => {
     {
       dataField: 'applicant_full_name',
       text: 'Name',
-      // filter: textFilter()
     },
     {
       dataField: 'applicant_program_name',
